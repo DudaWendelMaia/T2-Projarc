99ae2bc2eb942711f37c071f791227b38349031c
Maria Maia <113213701+DudaWendelMaia@users.noreply.github.com>
2024-11-07 10:47:36 -0300

    Add files via upload

---
+++ estoque-microsservicos/src/main/java/com/estoque/microsservicos/SistemaDeMonitoramentoEControleDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.estoque.microsservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMonitoramentoEControleDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeMonitoramentoEControleDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ estoque-microsservicos/src/test/java/com/estoque/microsservicos/SistemaDeMonitoramentoEControleDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.microsservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMonitoramentoEControleDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
